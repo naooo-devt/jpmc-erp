9810e30bf3c78342bb1032c8685a2b5147c3fe65
mspf24 <114278517+mspf24@users.noreply.github.com>
2025-08-08 15:51:48 +0800

    Delete sql/purchase_orders.sql

--- sql/purchase_orders.sql
+++
@@ -1,9 +0,0 @@
-SELECT po.id, po.order_number, po.order_date, po.expected_delivery, po.status, po.total_amount,
-       s.name as supplier_name, s.contact_person as supplier_contact,
-       COUNT(poi.id) as total_items
-FROM purchase_orders po
-LEFT JOIN suppliers s ON po.supplier_id = s.id
-LEFT JOIN purchase_order_items poi ON po.id = poi.purchase_order_id
-GROUP BY po.id
-ORDER BY po.order_date DESC
-LIMIT 10;
