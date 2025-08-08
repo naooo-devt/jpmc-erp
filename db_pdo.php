771934c04041af719dea92ba5942567c42134abe
mspf24 <114278517+mspf24@users.noreply.github.com>
2025-08-08 16:16:06 +0800

    Add files via upload

---
+++ db_pdo.php
@@ -0,0 +1,20 @@
+<?php
+// db_pdo.php
+$host = 'localhost';
+$db   = 'james_polymer_erp';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    die('PDO Connection failed: ' . $e->getMessage());
+}
+?>
